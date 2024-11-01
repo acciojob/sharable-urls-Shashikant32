4606febb161951686ccc2757b49aec2455309282
shashikant singh <114600939+Shashikant32@users.noreply.github.com>
2024-11-01 19:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// your code here
+document.getElementById('urlForm').addEventListener('submit', function(event) {
+event.preventDefault();
+	const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+
+	let queryString = '';
+            if (name) {
+                queryString += `?name=${encodeURIComponent(name)}`;
+            }
+            if (year) {
+                queryString += (queryString ? '&' : '?') + `year=${encodeURIComponent(year)}`;
+            }
+	 const baseUrl = 'https://localhost:8080/';
+            document.getElementById('url').innerText = baseUrl + queryString;
+        });
